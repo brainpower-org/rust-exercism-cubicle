87cc49996c3e6714b6c15f620c4c6d13e0c8b60f
Patrick Kollodzik <patrick.kollodzik@sinnerschrader.com>
2019-04-11 17:28:24 +0200

    feat: finished leap exercise

--- exercises/leap/src/lib.rs
+++ exercises/leap/src/lib.rs
@@ -1,3 +1,44 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+    // Solution 1
+    // year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+
+    // Solution 2
+    match (year % 4, year % 100, year % 400) {
+        (0, 0, 0) => true,
+        (0, 0, _) => false,
+        (0, _, _) => true,
+        _ => false
+    }
+
+    // Solution 3
+    // struct LeapYear {
+    //     _4: bool,
+    //     _100: bool,
+    //     _400: bool,
+    // }
+
+    // let year_struct = LeapYear {
+    //     _4: year % 4 == 0,
+    //     _100: year % 100 == 0,
+    //     _400: year % 400 == 0,
+    // };
+
+    // match year_struct {
+    //     LeapYear {
+    //         _4: true,
+    //         _100: true,
+    //         _400: true,
+    //     } => true,
+    //     LeapYear {
+    //         _4: true,
+    //         _100: false,
+    //         _400: false,
+    //     } => true,
+    //     LeapYear {
+    //         _4: true,
+    //         _100: true,
+    //         _400: false,
+    //     } => false,
+    //     _ => false,
+    // }
 }
