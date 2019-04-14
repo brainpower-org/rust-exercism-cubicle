a6c7a3357eb7568cffc2d917a2c322ca746bb9c4
Mario Nebl <hello@mario-nebl.de>
2019-04-14 13:30:55 +0200

    docs: move solution variants to doc tests

--- exercises/leap/src/lib.rs
+++ exercises/leap/src/lib.rs
@@ -1,7 +1,80 @@
+/// Solution 1
+/// ```
+/// pub fn is_leap_year(year: u64) -> bool {
+///     year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+/// }
+/// 
+/// # fn process_leapyear_case(year: u64, expected: bool) {
+/// #   assert_eq!(leap::is_leap_year(year), expected);
+/// # }
+/// 
+/// process_leapyear_case(1996, true);
+/// process_leapyear_case(2015, false);
+/// process_leapyear_case(1800, false);
+/// process_leapyear_case(2100, false);
+/// process_leapyear_case(2000, true);
+/// assert_eq!(leap::is_leap_year(1997), false);
+/// assert_eq!(leap::is_leap_year(1700), false);
+/// assert_eq!(leap::is_leap_year(1800), false);
+/// assert_eq!(leap::is_leap_year(1900), false);
+/// assert_eq!(leap::is_leap_year(1600), true);
+/// assert_eq!(leap::is_leap_year(2000), true);
+/// assert_eq!(leap::is_leap_year(2400), true);
+/// ```
+/// 
+/// Solution 3
+/// ```
+/// pub fn is_leap_year(year: u64) -> bool {
+///     struct LeapYear {
+///         _4: bool,
+///         _100: bool,
+///         _400: bool,
+///     }
+///
+///    let year_struct = LeapYear {
+///        _4: year % 4 == 0,
+///        _100: year % 100 == 0,
+///        _400: year % 400 == 0,
+///    };
+///
+///    match year_struct {
+///        LeapYear {
+///            _4: true,
+///            _100: true,
+///            _400: true,
+///        } => true,
+///        LeapYear {
+///            _4: true,
+///            _100: false,
+///            _400: false,
+///        } => true,
+///        LeapYear {
+///            _4: true,
+///            _100: true,
+///            _400: false,
+///        } => false,
+///        _ => false,
+///    }
+/// }
+/// 
+/// # fn process_leapyear_case(year: u64, expected: bool) {
+/// #   assert_eq!(leap::is_leap_year(year), expected);
+/// # }
+/// 
+/// process_leapyear_case(1996, true);
+/// process_leapyear_case(2015, false);
+/// process_leapyear_case(1800, false);
+/// process_leapyear_case(2100, false);
+/// process_leapyear_case(2000, true);
+/// assert_eq!(leap::is_leap_year(1997), false);
+/// assert_eq!(leap::is_leap_year(1700), false);
+/// assert_eq!(leap::is_leap_year(1800), false);
+/// assert_eq!(leap::is_leap_year(1900), false);
+/// assert_eq!(leap::is_leap_year(1600), true);
+/// assert_eq!(leap::is_leap_year(2000), true);
+/// assert_eq!(leap::is_leap_year(2400), true);
+/// ```
 pub fn is_leap_year(year: u64) -> bool {
-    // Solution 1
-    // year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
-
     // Solution 2
     match (year % 4, year % 100, year % 400) {
         (0, 0, 0) => true,
@@ -9,36 +82,4 @@ pub fn is_leap_year(year: u64) -> bool {
         (0, _, _) => true,
         _ => false
     }
-
-    // Solution 3
-    // struct LeapYear {
-    //     _4: bool,
-    //     _100: bool,
-    //     _400: bool,
-    // }
-
-    // let year_struct = LeapYear {
-    //     _4: year % 4 == 0,
-    //     _100: year % 100 == 0,
-    //     _400: year % 400 == 0,
-    // };
-
-    // match year_struct {
-    //     LeapYear {
-    //         _4: true,
-    //         _100: true,
-    //         _400: true,
-    //     } => true,
-    //     LeapYear {
-    //         _4: true,
-    //         _100: false,
-    //         _400: false,
-    //     } => true,
-    //     LeapYear {
-    //         _4: true,
-    //         _100: true,
-    //         _400: false,
-    //     } => false,
-    //     _ => false,
-    // }
 }
--- exercises/raindrops/src/lib.rs
+++ exercises/raindrops/src/lib.rs
@@ -1,25 +1,49 @@
+/// Solution 1
+/// ```rust
+/// pub fn raindrops(n: u32) -> String {
+///    let mut result = String::new();
+///
+///    if n % 3 == 0 {
+///        result.push_str("Pling");
+///    }
+///
+///    if n % 5 == 0 {
+///        result.push_str("Plang");
+///    }
+///
+///    if n % 7 == 0 {
+///        result.push_str("Plong");
+///    }
+///
+///    if result.is_empty() {
+///        n.to_string()
+///    } else {
+///        result
+///    }
+/// }
+/// 
+/// assert_eq!("1", raindrops::raindrops(1));
+/// assert_eq!("Pling", raindrops::raindrops(3));
+/// assert_eq!("Plang", raindrops::raindrops(5));
+/// assert_eq!("Plong", raindrops::raindrops(7));
+/// assert_eq!("Pling", raindrops::raindrops(6));
+/// assert_eq!("8", raindrops::raindrops(8));
+/// assert_eq!("Pling", raindrops::raindrops(9));
+/// assert_eq!("Plang", raindrops::raindrops(10));
+/// assert_eq!("Plong", raindrops::raindrops(14));
+/// assert_eq!("PlingPlang", raindrops::raindrops(15));
+/// assert_eq!("PlingPlong", raindrops::raindrops(21));
+/// assert_eq!("Plang", raindrops::raindrops(25));
+/// assert_eq!("Pling", raindrops::raindrops(27));
+/// assert_eq!("PlangPlong", raindrops::raindrops(35));
+/// assert_eq!("Plong", raindrops::raindrops(49));
+/// assert_eq!("52", raindrops::raindrops(52));
+/// assert_eq!("PlingPlangPlong", raindrops::raindrops(105));
+/// assert_eq!("Plang", raindrops::raindrops(3125));
+/// assert_eq!("12121", raindrops::raindrops(12121));
+/// ```
+/// 
 pub fn raindrops(n: u32) -> String {
-    // Solution 1
-    // let mut result = String::new();
-
-    // if n % 3 == 0 {
-    //     result.push_str("Pling");
-    // }
-
-    // if n % 5 == 0 {
-    //     result.push_str("Plang");
-    // }
-
-    // if n % 7 == 0 {
-    //     result.push_str("Plong");
-    // }
-
-    // if result.is_empty() {
-    //     n.to_string()
-    // } else {
-    //     result
-    // }
-
     // Solution 2
     let result = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
         .into_iter()
@@ -33,3 +57,4 @@ pub fn raindrops(n: u32) -> String {
         result
     }
 }
+
