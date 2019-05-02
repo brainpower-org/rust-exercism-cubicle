b7f603d4d62b9bdb784b0a6a1d23feda14fda8de
Kollode <patrick.kollodzik@googlemail.com>
2019-05-02 18:14:40 +0200

    feat: add beer-song (#4)
    * feat: add beer-song
    * test: enable tests

--- exercises/beer-song/src/lib.rs
+++ exercises/beer-song/src/lib.rs
@@ -1,7 +1,13 @@
 pub fn verse(n: i32) -> String {
-    unimplemented!("emit verse {}", n)
+    match dbg!(n) {
+        0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
+        1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
+        2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".to_string(),
+        n @ 3...99 => format!("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, {m} bottles of beer on the wall.\n", n=n, m=(n-1)),
+        _ => unreachable!()
+    }
 }
 
 pub fn sing(start: i32, end: i32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    (end..=start).rev().map(verse).collect::<Vec<String>>().join("\n")
 }
