dd5352e7298d5339281fe7d5d12d3adafd744ffe
Mario Nebl <hello@mario-nebl.de>
2019-05-03 08:40:03 +0200

    feat: enable lldb debugging via vscode

--- exercises/beer-song/src/lib.rs
+++ exercises/beer-song/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn verse(n: i32) -> String {
-    match dbg!(n) {
+    match n {
         0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
         1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
         2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".to_string(),
