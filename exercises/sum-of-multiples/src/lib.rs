527b3229f4337465abbd91d59f320766acf60292
Mario Nebl <hello@mario-nebl.de>
2019-05-09 15:54:53 +0000

    implement sum of multiples

--- exercises/sum-of-multiples/src/lib.rs
+++ exercises/sum-of-multiples/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    (1..limit)
+        .filter(|x| factors.iter().filter(|y| *y > &0).any(|y| x % y == 0))
+        .sum()
 }
+
+// TODO implement imperative solution
