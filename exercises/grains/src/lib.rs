41e5d8321b92c5dd2ce8e1aecf15dd67a9c513ce
Mario Nebl <hello+github.com@mario-nebl.de>
2019-05-17 17:55:34 +0000

    fix compose file

--- exercises/grains/src/lib.rs
+++ exercises/grains/src/lib.rs
@@ -1,7 +1,13 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    if s < 1 || s > 64 {
+        panic!("Square must be between 1 and 64");
+    } else {
+        2u64.pow(s - 1)
+    }
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..=64)
+        .map(square)
+        .sum()
 }
