a5e4a6484e9438c3a39c4befda19907db99587d8
Ronny Gärtner <ronny.gaertner@sinnerschrader.com>
2019-06-20 18:15:12 +0200

    WIP: exercise bob solved, need to benchmark imperative version

---
+++ exercises/bob/benches/benchmark.rs
@@ -0,0 +1,27 @@
+#![feature(test)]
+extern crate bob;
+extern crate test;
+
+use test::Bencher;
+
+#[bench]
+fn bench_lorem_collect(b: &mut Bencher) {
+    let lorem = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+    b.iter(|| bob::shouting(lorem));
+}
+
+#[bench]
+fn bench_lorem_peek(b: &mut Bencher) {
+    let lorem = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+    b.iter(|| bob::shouting_peek(lorem));
+}
+
+#[bench]
+fn bench_a_collect(b: &mut Bencher) {
+    b.iter(|| bob::shouting("A"));
+}
+
+#[bench]
+fn bench_a_peek(b: &mut Bencher) {
+    b.iter(|| bob::shouting_peek("A"));
+}
--- exercises/bob/src/lib.rs
+++ exercises/bob/src/lib.rs
@@ -1,3 +1,41 @@
-pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+pub fn shouting(message: &str) -> bool {
+    let letters = message.chars()
+        .filter(|c: &char| c.is_alphabetic())
+        .collect::<String>();
+
+    letters.len() > 0 && letters.to_uppercase() == letters
+}
+
+pub fn shouting_peek(message: &str) -> bool {
+    let mut letters = message.chars()
+        .filter(|c: &char| c.is_alphabetic());
+        
+    letters.peek().is_some() && letters.all(|c: char| c.is_uppercase())
+}
+
+pub fn shouting_imperative(message: &str) -> bool {
+    let result = true;
+    for c in message.it {
+        c
+    }
+    result
+}
+
+pub fn reply(message: &str) -> &str { 
+    let message = message.trim();
+    let shouts = shouting_peek(message);
+    let asks = message.ends_with("?");
+    let empty = message.len() == 0;
+
+    if shouts && asks {
+        "Calm down, I know what I'm doing!"
+    } else if empty {
+        "Fine. Be that way!"
+    } else if asks {
+        "Sure."
+    } else if shouts {
+        "Whoa, chill out!"
+    } else {
+        "Whatever."
+    }
 }
