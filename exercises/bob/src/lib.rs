d6c013a3935c388b8ee8bf19b09437845dd8d9ff
Ronny Gärtner <506777+rgaertner@users.noreply.github.com>
2019-07-04 17:28:08 +0200

    implement bob
    * WIP: exercise bob solved, need to benchmark imperative version
    
    * implement and bench imperative version

--- exercises/bob/src/lib.rs
+++ exercises/bob/src/lib.rs
@@ -1,3 +1,69 @@
-pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+pub fn shouting(message: &str) -> bool {
+    let letters = message.chars()
+        .filter(|c: &char| c.is_alphabetic())
+        .collect::<String>();
+
+    letters.len() > 0 && letters.to_uppercase() == letters
+}
+
+pub fn shouting_peek(message: &str) -> bool {
+    let mut letters = message.chars()
+        .filter(|c: &char| c.is_alphabetic())
+        .peekable();
+        
+    letters.peek().is_some() && letters.all(|c: char| c.is_uppercase())
+}
+
+pub fn shouting_imperative(message: &str) -> bool {
+    let mut hit = false;
+    let mut result = true;
+
+    for c in message.chars() {
+        hit = true;
+
+        if c.is_alphabetic() && c.is_lowercase() {
+            result = false;
+            break;
+        }
+    }
+
+    hit && result
+}
+
+pub fn reply(message: &str) -> &str { 
+    let message = message.trim();
+    let shouts = shouting_peek(message);
+    let asks = message.ends_with("?");
+    let empty = message.len() == 0;
+
+    if shouts && asks {
+        "Calm down, I know what I'm doing!"
+    } else if empty {
+        "Fine. Be that way!"
+    } else if asks {
+        "Sure."
+    } else if shouts {
+        "Whoa, chill out!"
+    } else {
+        "Whatever."
+    }
+}
+
+pub fn reply_imperative(message: &str) -> &str { 
+    let message = message.trim();
+    let shouts = shouting_imperative(message);
+    let asks = message.ends_with("?");
+    let empty = message.len() == 0;
+
+    if shouts && asks {
+        "Calm down, I know what I'm doing!"
+    } else if empty {
+        "Fine. Be that way!"
+    } else if asks {
+        "Sure."
+    } else if shouts {
+        "Whoa, chill out!"
+    } else {
+        "Whatever."
+    }
 }
--- exercises/bob/tests/bob.rs
+++ exercises/bob/tests/bob.rs
@@ -4,56 +4,51 @@ fn process_response_case(phrase: &str, expected_response: &str) {
     assert_eq!(bob::reply(phrase), expected_response);
 }
 
+fn process_response_case_imperative(phrase: &str, expected_response: &str) {
+    assert_eq!(bob::reply_imperative(phrase), expected_response);
+}
+
 #[test]
-#[ignore]
 fn test_stating_something() {
     process_response_case("Tom-ay-to, tom-aaaah-to.", "Whatever.");
 }
 
 #[test]
-#[ignore]
 fn test_shouting() {
     process_response_case("WATCH OUT!", "Whoa, chill out!");
 }
 
 #[test]
-#[ignore]
 fn test_shouting_gibberish() {
     process_response_case("FCECDFCAAB", "Whoa, chill out!");
 }
 
 #[test]
-#[ignore]
 fn test_asking_a_question() {
     process_response_case("Does this cryogenic chamber make me look fat?", "Sure.");
 }
 
 #[test]
-#[ignore]
 fn test_asking_a_numeric_question() {
     process_response_case("You are, what, like 15?", "Sure.");
 }
 
 #[test]
-#[ignore]
 fn test_asking_gibberish() {
     process_response_case("fffbbcbeab?", "Sure.");
 }
 
 #[test]
-#[ignore]
 fn test_talking_forcefully() {
     process_response_case("Let's go make out behind the gym!", "Whatever.");
 }
 
 #[test]
-#[ignore]
 fn test_using_acronyms_in_regular_speech() {
     process_response_case("It's OK if you don't want to go to the DMV.", "Whatever.");
 }
 
 #[test]
-#[ignore]
 fn test_forceful_question() {
     process_response_case(
         "WHAT THE HELL WERE YOU THINKING?",
@@ -62,25 +57,21 @@ fn test_forceful_question() {
 }
 
 #[test]
-#[ignore]
 fn test_shouting_numbers() {
     process_response_case("1, 2, 3 GO!", "Whoa, chill out!");
 }
 
 #[test]
-#[ignore]
 fn test_no_letters() {
     process_response_case("1, 2, 3", "Whatever.");
 }
 
 #[test]
-#[ignore]
 fn test_question_with_no_letters() {
     process_response_case("4?", "Sure.");
 }
 
 #[test]
-#[ignore]
 fn test_shouting_with_special_characters() {
     process_response_case(
         "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!",
@@ -89,49 +80,41 @@ fn test_shouting_with_special_characters() {
 }
 
 #[test]
-#[ignore]
 fn test_shouting_with_no_exclamation_mark() {
     process_response_case("I HATE THE DMV", "Whoa, chill out!");
 }
 
 #[test]
-#[ignore]
 fn test_statement_containing_question_mark() {
     process_response_case("Ending with ? means a question.", "Whatever.");
 }
 
 #[test]
-#[ignore]
 fn test_nonletters_with_question() {
     process_response_case(":) ?", "Sure.");
 }
 
 #[test]
-#[ignore]
 fn test_prattling_on() {
     process_response_case("Wait! Hang on. Are you going to be OK?", "Sure.");
 }
 
 #[test]
-#[ignore]
 fn test_silence() {
     process_response_case("", "Fine. Be that way!");
 }
 
 #[test]
-#[ignore]
 fn test_prolonged_silence() {
     process_response_case("          ", "Fine. Be that way!");
 }
 
 #[test]
-#[ignore]
 fn test_alternate_silence() {
     process_response_case("\t\t\t\t\t\t\t\t\t\t", "Fine. Be that way!");
 }
 
 #[test]
-#[ignore]
 fn test_multiple_line_question() {
     process_response_case(
         "\nDoes this cryogenic chamber make me look fat?\nNo.",
@@ -140,28 +123,70 @@ fn test_multiple_line_question() {
 }
 
 #[test]
-#[ignore]
 fn test_starting_with_whitespace() {
     process_response_case("         hmmmmmmm...", "Whatever.");
 }
 
 #[test]
-#[ignore]
 fn test_ending_with_whitespace() {
     process_response_case("Okay if like my  spacebar  quite a bit?   ", "Sure.");
 }
 
 #[test]
-#[ignore]
 fn test_other_whitespace() {
     process_response_case("\n\r \t", "Fine. Be that way!");
 }
 
 #[test]
-#[ignore]
 fn test_nonquestion_ending_with_whitespace() {
     process_response_case(
         "This is a statement ending with whitespace      ",
         "Whatever.",
     );
+}
+
+#[test]
+fn test_shouting_imperative() {
+    process_response_case_imperative("WATCH OUT!", "Whoa, chill out!");
+}
+
+#[test]
+fn test_shouting_gibberish_imperative() {
+    process_response_case_imperative("FCECDFCAAB", "Whoa, chill out!");
+}
+
+#[test]
+fn test_talking_forcefully_imperative() {
+    process_response_case_imperative("Let's go make out behind the gym!", "Whatever.");
+}
+
+#[test]
+fn test_using_acronyms_in_regular_speech_imperative() {
+    process_response_case_imperative("It's OK if you don't want to go to the DMV.", "Whatever.");
+}
+
+#[test]
+fn test_forceful_question_imperative() {
+    process_response_case_imperative(
+        "WHAT THE HELL WERE YOU THINKING?",
+        "Calm down, I know what I'm doing!",
+    );
+}
+
+#[test]
+fn test_shouting_numbers_imperative() {
+    process_response_case_imperative("1, 2, 3 GO!", "Whoa, chill out!");
+}
+
+#[test]
+fn test_shouting_with_special_characters_imperative() {
+    process_response_case_imperative(
+        "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!",
+        "Whoa, chill out!",
+    );
+}
+
+#[test]
+fn test_shouting_with_no_exclamation_mark_imperative() {
+    process_response_case_imperative("I HATE THE DMV", "Whoa, chill out!");
 }
