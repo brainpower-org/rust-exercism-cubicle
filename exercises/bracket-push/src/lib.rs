09d5c224e601303f7aa7dc693ea786c04c298ada
Daviti Nalchevanidze <daviti.nalchevanidze@sinnerschrader.com>
2019-07-11 19:58:01 +0200

    alternative implementation of brackets-push

--- exercises/bracket-push/src/lib.rs
+++ exercises/bracket-push/src/lib.rs
@@ -1,34 +1,70 @@
-pub fn is_opening (c: char) -> bool {
-    match c { 
-        '[' | '(' | '{' => true,
-        _ => false
+/// ```
+/// pub fn is_bracket (c: char) -> bool {
+///     match c {
+///         '[' | ']' | '(' | ')' | '{' | '}' => true,
+///         _ => false
+///     }
+/// }
+///
+/// pub fn brackets_are_balanced(string: &str) -> bool {
+///     string
+///         .chars()
+///         .filter(|c| is_bracket(*c))
+///         .fold(vec![], |mut acc, c| {
+///             match (c, acc.last()) {
+///                ('[', _) | ('(', _) | ('{', _) => { acc.push(c); acc },
+///                (']', Some('[')) | ('}', Some('{')) | (')', Some('(')) => {
+///                    acc.pop();
+///                    acc
+///                },
+///                (']', _) | ('}', _) | (')', _) => {
+///                    acc.push(c);
+///                    acc
+///                },
+///                _ => acc
+///             }
+///         })
+///         .len() == 0
+/// }
+/// ```
+///
+///
+
+pub fn is_bracket(c: &char) -> bool {
+    match c {
+        '[' | '(' | '{' | ']' | ')' | '}' => true,
+        _ => false,
     }
 }
 
-pub fn is_bracket (c: char) -> bool {
-    match c { 
-        '[' | ']' | '(' | ')' | '{' | '}' => true,
-        _ => false
+pub fn is_opening(c: char) -> Option<char> {
+    match c {
+        '[' => Some(']'),
+        '(' => Some(')'),
+        '{' => Some('}'),
+        _ => None,
     }
 }
 
 pub fn brackets_are_balanced(string: &str) -> bool {
     string
         .chars()
-        .filter(|c| is_bracket(*c))
-        .fold(vec![], |mut acc, c| {
-            match (c, acc.last()) {
-               ('[', _) | ('(', _) | ('{', _) => { acc.push(c); acc },
-               (']', Some('[')) | ('}', Some('{')) | (')', Some('(')) => {
-                   acc.pop();
-                   acc
-               },
-               (']', _) | ('}', _) | (')', _) => {
-                   acc.push(c); 
-                   acc
-               },
-               _ => acc
-            }
+        .filter(is_bracket)
+        .fold(Some(&mut vec![]), |mut stack, c| {
+            match (c, stack.as_mut()) {
+                ('[', Some(s)) => s.push(']'),
+                ('(', Some(s)) => s.push(')'),
+                ('{', Some(s)) => s.push('}'),
+                (x, Some(s)) => {
+                    if s.last() == Some(&x) {
+                        s.pop();
+                    } else {
+                        stack = None;
+                    }
+                }
+                (_, _) => (),
+            };
+            stack
         })
-        .len() == 0
+        .map_or(false, |s| s.iter().peekable().peek().is_none())
 }
