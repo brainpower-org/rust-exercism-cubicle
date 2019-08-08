6769caa928fad64928641352d3ee3da66c122b44
Mario Nebl <hello+github.com@mario-nebl.de>
2019-08-08 17:06:05 +0200

    reorganize code into mod/struct

--- exercises/bracket-push/src/lib.rs
+++ exercises/bracket-push/src/lib.rs
@@ -1,11 +1,11 @@
-use std::iter::FromIterator;
 use std::convert::TryFrom;
+use std::iter::FromIterator;
 
 #[derive(Eq, PartialEq)]
 pub enum Bracket {
     Round,
     Curly,
-    Square
+    Square,
 }
 
 pub enum BracketType {
@@ -14,32 +14,35 @@ pub enum BracketType {
 }
 
 pub fn brackets_are_balanced(string: &str) -> bool {
-    string.chars().collect::<BracketList>().0.into_iter()
-        .fold(Ok(vec![]), |stack, b| stack.and_then(|s| process(b, s)))
-        .ok()
-       .map_or(false, |s| s.is_empty())
+    string.chars().collect::<bracket_list::BracketList>().is_balanced()
 }
 
 pub fn process(b: BracketType, mut s: Vec<Bracket>) -> Result<Vec<Bracket>, Vec<Bracket>> {
     match (b, s.last()) {
-        (BracketType::Opening(b), _) => { s.push(b); Ok(s) },
-        (BracketType::Closing(ref c), Some(d)) if c == d => { s.pop(); Ok(s) },
-        _ => { Err(s) }
+        (BracketType::Opening(b), _) => {
+            s.push(b);
+            Ok(s)
+        }
+        (BracketType::Closing(ref c), Some(d)) if c == d => {
+            s.pop();
+            Ok(s)
+        }
+        _ => Err(s),
     }
 }
 
 pub fn to_bracket(c: char) -> Option<BracketType> {
-    use BracketType::*;
     use Bracket::*;
+    use BracketType::*;
 
     match c {
-        | '[' => Some(Opening(Square)),
-        | '(' => Some(Opening(Round)),
-        | '{' => Some(Opening(Curly)),
-        | ')' => Some(Closing(Round)),
-        | ']' => Some(Closing(Square)),
-        | '}' => Some(Closing(Curly)),
-        _ => None
+        '[' => Some(Opening(Square)),
+        '(' => Some(Opening(Round)),
+        '{' => Some(Opening(Curly)),
+        ')' => Some(Closing(Round)),
+        ']' => Some(Closing(Square)),
+        '}' => Some(Closing(Curly)),
+        _ => None,
     }
 }
 
@@ -48,32 +51,48 @@ pub fn get_closing(c: char) -> Option<char> {
         '[' => Some(']'),
         '(' => Some(')'),
         '{' => Some('}'),
-        _ => None
+        _ => None,
     }
 }
-
 
 impl TryFrom<char> for BracketType {
     type Error = &'static str;
 
     fn try_from(c: char) -> Result<Self, Self::Error> {
-       to_bracket(c).ok_or("No Bracket")
+        to_bracket(c).ok_or("No Bracket")
     }
 }
 
-struct BracketList(Vec<BracketType>);
+mod bracket_list {
+    use super::*;
 
+    pub struct BracketList {
+        brackets: Vec<BracketType>,
+    }
 
-impl FromIterator<char> for BracketList {
-    
-
-    fn from_iter<I: IntoIterator<Item=char>>(iter: I) -> Self {
-        let mut result = vec![];
-        for i in iter {
-            if let Ok(r) = BracketType::try_from(i) {
-                result.push(r);
-            }
+    impl BracketList {
+        pub fn new(brackets: Vec<BracketType>) -> BracketList {
+            BracketList { brackets }
         }
-        BracketList(result)
+
+        pub fn is_balanced(self: BracketList) -> bool {
+            self.brackets
+                .into_iter()
+                .fold(Ok(vec![]), |stack, b| stack.and_then(|s| process(b, s)))
+                .ok()
+                .map_or(false, |s| s.is_empty())
+        }
+    }
+
+    impl FromIterator<char> for BracketList {
+        fn from_iter<I: IntoIterator<Item = char>>(iter: I) -> Self {
+            let mut result = vec![];
+            for i in iter {
+                if let Ok(r) = BracketType::try_from(i) {
+                    result.push(r);
+                }
+            }
+            BracketList::new(result)
+        }
     }
 }
