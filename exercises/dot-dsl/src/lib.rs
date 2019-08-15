7763dee5f2e6e5e57b11159491c2be252253a0f1
rsteube <rsteube@users.noreply.github.com>
2019-08-15 18:10:17 +0200

    started working on dot-dsl

--- exercises/dot-dsl/src/lib.rs
+++ exercises/dot-dsl/src/lib.rs
@@ -1,9 +1,84 @@
-pub mod graph {
-    pub struct Graph;
+#[macro_use]
+extern crate maplit;
 
-    impl Graph {
+pub mod graph {
+    pub type Attribute<'a> = (&'a str, &'a str);
+
+    pub mod graph_items {
+        pub mod edge {
+            #[derive(PartialEq, Debug, Clone)]
+            pub struct Edge<'a> {
+                from: &'a str,
+                to: &'a str,
+            }
+
+            impl<'a> Edge<'a> {
+                pub fn new(from: &'a str, to: &'a str) -> Self {
+                    Edge { from, to }
+                }
+            }
+        }
+
+        pub mod node {
+            use super::super::Attribute;
+
+            #[derive(PartialEq, Debug, Clone)]
+            pub struct Node<'a> {
+                pub attrs: Vec<Attribute<'a>>,
+            }
+
+            impl<'a> Node<'a> {
+                pub fn new(name: &str) -> Self {
+                    // TODO store name
+                    Node {
+                        // TODO maybe store as hashmap
+                        attrs: vec![],
+                    }
+                }
+
+                pub fn with_attrs(mut self, attrs: &[(&'a str, &'a str)]) -> Self {
+                    self.attrs = attrs.to_vec();
+                    self
+                }
+            }
+        }
+    }
+
+    use graph_items::edge::Edge;
+    use graph_items::node::Node;
+    use std::collections::HashMap;
+
+    pub struct Graph<'a> {
+        pub nodes: Vec<Node<'a>>,
+        pub edges: Vec<Edge<'a>>,
+        pub attrs: HashMap<String, String>,
+    }
+
+    impl<'a> Graph<'a> {
         pub fn new() -> Self {
-            unimplemented!("Construct a new Graph struct.");
+            Graph {
+                nodes: vec![],
+                edges: vec![],
+                attrs: hashmap! {},
+            }
+        }
+
+        pub fn with_nodes(mut self, nodes: &Vec<Node<'a>>) -> Self {
+            self.nodes = nodes.to_vec();
+            self
+        }
+
+        pub fn with_edges(mut self, edges: &Vec<Edge<'a>>) -> Self {
+            self.edges = edges.to_vec();
+            self
+        }
+
+        pub fn with_attrs(mut self, attrs: &[Attribute<'a>]) -> Self {
+            self.attrs = attrs
+                .iter()
+                .map(|(key, value)| (key.to_string(), value.to_string()))
+                .collect();
+            self
         }
     }
 }
