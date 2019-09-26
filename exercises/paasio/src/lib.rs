cae47ad50a4023e1a1f16477739843458c2fce80
Patrick Kollodzik <patrick.kollodzik@sinnerschrader.com>
2019-09-26 17:39:40 +0200

    Add passio implementation

--- exercises/paasio/src/lib.rs
+++ exercises/paasio/src/lib.rs
@@ -1,63 +1,89 @@
 use std::io::{Read, Result, Write};
 
-pub struct ReadStats<R>(::std::marker::PhantomData<R>);
+pub struct ReadStats<R> {
+    data: R,
+    number_of_reads: usize,
+    number_of_bytes: usize
+}
 
 impl<R: Read> ReadStats<R> {
     // _wrapped is ignored because R is not bounded on Debug or Display and therefore
     // can't be passed through format!(). For actual implementation you will likely
     // wish to remove the leading underscore so the variable is not ignored.
-    pub fn new(_wrapped: R) -> ReadStats<R> {
-        unimplemented!()
+    pub fn new(wrapped: R) -> ReadStats<R> {
+        ReadStats {
+            data: wrapped,
+            number_of_reads: 0,
+            number_of_bytes: 0
+        }
     }
 
     pub fn get_ref(&self) -> &R {
-        unimplemented!()
+        &self.data
     }
 
     pub fn bytes_through(&self) -> usize {
-        unimplemented!()
+        self.number_of_bytes
     }
 
     pub fn reads(&self) -> usize {
-        unimplemented!()
+        self.number_of_reads
     }
 }
 
 impl<R: Read> Read for ReadStats<R> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        unimplemented!("Collect statistics about this call reading {:?}", buf)
+        self.number_of_reads += 1;
+
+        let result = self.data.read(buf)?;
+        self.number_of_bytes += result;
+
+        Ok(result)
     }
 }
 
-pub struct WriteStats<W>(::std::marker::PhantomData<W>);
+pub struct WriteStats<W> {
+    data: W,
+    number_of_writes: usize,
+    number_of_bytes: usize
+}
 
 impl<W: Write> WriteStats<W> {
     // _wrapped is ignored because W is not bounded on Debug or Display and therefore
     // can't be passed through format!(). For actual implementation you will likely
     // wish to remove the leading underscore so the variable is not ignored.
-    pub fn new(_wrapped: W) -> WriteStats<W> {
-        unimplemented!()
+    pub fn new(wrapped: W) -> WriteStats<W> {
+        WriteStats {
+            data: wrapped,
+            number_of_writes: 0,
+            number_of_bytes: 0
+        }
     }
 
     pub fn get_ref(&self) -> &W {
-        unimplemented!()
+        &self.data
     }
 
     pub fn bytes_through(&self) -> usize {
-        unimplemented!()
+        self.number_of_bytes
     }
 
     pub fn writes(&self) -> usize {
-        unimplemented!()
+        self.number_of_writes
     }
 }
 
 impl<W: Write> Write for WriteStats<W> {
     fn write(&mut self, buf: &[u8]) -> Result<usize> {
-        unimplemented!("Collect statistics about this call writing {:?}", buf)
+        self.number_of_writes += 1;
+
+        let result = self.data.write(buf)?;
+        self.number_of_bytes += result;
+
+        Ok(result)
     }
 
     fn flush(&mut self) -> Result<()> {
-        unimplemented!()
+        self.data.flush()
     }
 }
--- exercises/paasio/tests/paasio.rs
+++ exercises/paasio/tests/paasio.rs
@@ -154,18 +154,17 @@ macro_rules! test_write {
 }
 
 #[test]
-#[ignore]
 fn test_create_stats() {
     let mut data: Vec<u8> = Vec::new();
     let _ = paasio::ReadStats::new(data.as_slice());
     let _ = paasio::WriteStats::new(data.as_mut_slice());
 }
 
-test_read!(#[ignore] read_string (
+test_read!(read_string (
     "Twas brillig, and the slithy toves/Did gyre and gimble in the wabe:/All mimsy were the borogoves,/And the mome raths outgrabe.".as_bytes(),
     |d: &[u8]| d.len()
 ));
-test_write!(#[ignore] write_string (
+test_write!(write_string (
     "Beware the Jabberwock, my son!/The jaws that bite, the claws that catch!/Beware the Jubjub bird, and shun/The frumious Bandersnatch!".as_bytes(),
     |d: &[u8]| d.len()
 ));
