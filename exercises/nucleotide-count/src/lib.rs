2113caa39b5910deaed05ab47a70d0f319eb4495
Daviti Nalchevanidze <daviti.nalchevanidze@sinnerschrader.com>
2019-10-10 18:22:35 +0200

    implement nucleotide count

--- exercises/nucleotide-count/src/lib.rs
+++ exercises/nucleotide-count/src/lib.rs
@@ -1,11 +1,22 @@
 use std::collections::HashMap;
+use std::str::FromStr;
+
+const BASES: [char; 4] = ['A','C','T','G'];
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    unimplemented!(
-        "How much of nucleotide type '{}' is contained inside DNA string '{}'?",
-        nucleotide,
-        dna
-    );
+    // next iteration use fold!
+    let dna_strand = String::from_str(dna).unwrap();
+    if BASES.contains(&nucleotide) {
+
+        let error_base = dna_strand.chars().filter(|c: &char| !BASES.contains(&c)).next();
+        
+        if let Some(error_base) = error_base {
+            return Err(error_base);
+        }
+        let matches = dna_strand.chars().filter(|c: &char| *c == nucleotide);
+        return Ok(matches.count()); 
+    }
+    Err(nucleotide)
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
--- exercises/nucleotide-count/tests/nucleotide-count.rs
+++ exercises/nucleotide-count/tests/nucleotide-count.rs
@@ -17,37 +17,31 @@ fn process_nucleotidecounts_case(s: &str, pairs: &[(char, usize)]) {
 }
 
 #[test]
-#[ignore]
 fn count_returns_result() {
     assert!(dna::count('A', "").is_ok());
 }
 
 #[test]
-#[ignore]
 fn test_count_empty() {
     assert_eq!(dna::count('A', ""), Ok(0));
 }
 
 #[test]
-#[ignore]
 fn count_invalid_nucleotide() {
     assert_eq!(dna::count('X', "A"), Err('X'));
 }
 
 #[test]
-#[ignore]
 fn count_invalid_dna() {
     assert_eq!(dna::count('A', "AX"), Err('X'));
 }
 
 #[test]
-#[ignore]
 fn test_count_repetitive_cytosine() {
     assert_eq!(dna::count('C', "CCCCC"), Ok(5));
 }
 
 #[test]
-#[ignore]
 fn test_count_only_thymine() {
     assert_eq!(dna::count('T', "GGGGGTAACCCGG"), Ok(1));
 }
