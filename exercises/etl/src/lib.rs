859a23a3bc9d02e65b40e4286ff983b6507b8a4f
Ronny Gärtner <ronny.gaertner@sinnerschrader.com>
2019-11-07 17:41:59 +0100

    solve etl

--- exercises/etl/src/lib.rs
+++ exercises/etl/src/lib.rs
@@ -1,5 +1,9 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    unimplemented!("How will you transform the tree {:?}?", h)
+    h.iter().fold(BTreeMap::new(), | acc, (key, value) | {
+        value.iter().fold(acc, |mut acc2, v| { 
+            acc2.entry((*v).to_lowercase().next().unwrap()).or_insert(*key); acc2 
+        }) 
+    } )
 }
