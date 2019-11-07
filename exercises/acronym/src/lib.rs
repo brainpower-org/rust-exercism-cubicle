7b892eaf0d821092e737efc28d8f9ecd7dbeeaa3
Ronny Gärtner <ronny.gaertner@sinnerschrader.com>
2019-11-07 18:44:40 +0100

    WIP first acronym implementation

--- exercises/acronym/src/lib.rs
+++ exercises/acronym/src/lib.rs
@@ -1,3 +1,21 @@
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+    let splitted = phrase.split(|c: char| [' ', '-'].contains(&c));
+
+    splitted.filter_map(|w| {
+        let mut word = w.chars().filter(|c| c.is_ascii_alphanumeric());
+        match word.next() {
+        None => None,
+        Some(c) => {
+            if c.is_ascii_lowercase() {
+                return Some(c.to_ascii_uppercase().to_string());
+            } else {
+                let filtered: String = word.clone().filter(|d| d.is_ascii_uppercase()).collect();
+                if filtered.len() == word.count() {
+                    return Some(c.to_string());
+                }
+                Some(filtered)
+            }
+        }
+        }
+    }).collect()
 }
--- exercises/acronym/tests/acronym.rs
+++ exercises/acronym/tests/acronym.rs
@@ -1,37 +1,31 @@
 use acronym;
 
 #[test]
-#[ignore]
 fn empty() {
     assert_eq!(acronym::abbreviate(""), "");
 }
 
 #[test]
-#[ignore]
 fn basic() {
     assert_eq!(acronym::abbreviate("Portable Network Graphics"), "PNG");
 }
 
 #[test]
-#[ignore]
 fn lowercase_words() {
     assert_eq!(acronym::abbreviate("Ruby on Rails"), "ROR");
 }
 
 #[test]
-#[ignore]
 fn camelcase() {
     assert_eq!(acronym::abbreviate("HyperText Markup Language"), "HTML");
 }
 
 #[test]
-#[ignore]
 fn punctuation() {
     assert_eq!(acronym::abbreviate("First In, First Out"), "FIFO");
 }
 
 #[test]
-#[ignore]
 fn all_caps_words() {
     assert_eq!(acronym::abbreviate("PHP: Hypertext Preprocessor"), "PHP");
 }
