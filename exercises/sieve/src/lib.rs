20bf012ed5a83dd3742e9000ab970a782dceaa31
Patrick Kollodzik <patrick.kollodzik@sinnerschrader.com>
2019-11-14 17:51:46 +0100

    first sieve implementation

--- exercises/sieve/src/lib.rs
+++ exercises/sieve/src/lib.rs
@@ -1,3 +1,38 @@
+use std::collections::HashMap;
+
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    unimplemented!("Construct a vector of all primes up to {}", upper_bound);
+    let vec: Vec<u64> = Vec::new();
+    let mut primes: HashMap<u64, bool> = HashMap::new();
+
+    if upper_bound < 2 {
+        return vec;
+    }
+
+    (2..=upper_bound).for_each(|c| {
+        primes.insert(c, true);
+    });
+
+    (2..=upper_bound).for_each(|number| {
+        if !*primes.get(&number).unwrap() {
+            return ();
+        }
+
+        for x in ((number * 2)..=upper_bound).step_by(number as usize) {
+            primes.insert(x, false);
+        }
+    });
+
+    let mut result = primes
+        .iter()
+        .filter_map(|(number, prime)| {
+            if *prime {
+                return Some(*number);
+            }
+
+            None
+        })
+        .collect::<Vec<u64>>();
+    result.sort();
+    result
+        
 }
