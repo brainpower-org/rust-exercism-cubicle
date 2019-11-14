d04a17da8c45709f4dbb6d99170238e227ec4857
Patrick Kollodzik <patrick.kollodzik@sinnerschrader.com>
2019-11-14 18:23:15 +0100

    second version of sieve

--- exercises/sieve/src/lib.rs
+++ exercises/sieve/src/lib.rs
@@ -1,38 +1,11 @@
-use std::collections::HashMap;
-
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    let vec: Vec<u64> = Vec::new();
-    let mut primes: HashMap<u64, bool> = HashMap::new();
-
-    if upper_bound < 2 {
-        return vec;
-    }
-
-    (2..=upper_bound).for_each(|c| {
-        primes.insert(c, true);
-    });
-
-    (2..=upper_bound).for_each(|number| {
-        if !*primes.get(&number).unwrap() {
-            return ();
-        }
-
-        for x in ((number * 2)..=upper_bound).step_by(number as usize) {
-            primes.insert(x, false);
-        }
-    });
-
-    let mut result = primes
-        .iter()
-        .filter_map(|(number, prime)| {
-            if *prime {
-                return Some(*number);
+    (2..=upper_bound).fold(
+        (2..=upper_bound).into_iter().collect::<Vec<u64>>(),
+        |mut acc, number| {
+            for x in ((number * 2)..=upper_bound).step_by(number as usize) {
+                acc = acc.into_iter().filter(|c| *c != x).collect();
             }
-
-            None
-        })
-        .collect::<Vec<u64>>();
-    result.sort();
-    result
-        
+            acc
+        },
+    )
 }
