aa2ef558c91b060d5f6d3ba86a4ab1c2504a9aa4
Ronny Gärtner <ronny.gaertner@sinnerschrader.com>
2019-11-21 18:16:40 +0100

    solve rna-transcription

--- exercises/rna-transcription/tests/rna-transcription.rs
+++ exercises/rna-transcription/tests/rna-transcription.rs
@@ -1,19 +1,16 @@
 use rna_transcription as dna;
 
 #[test]
-#[ignore]
 fn test_valid_dna_input() {
     assert!(dna::DNA::new("GCTA").is_ok());
 }
 
 #[test]
-#[ignore]
 fn test_valid_rna_input() {
     assert!(dna::RNA::new("CGAU").is_ok());
 }
 
 #[test]
-#[ignore]
 fn test_invalid_dna_input() {
     // Invalid character
     assert_eq!(dna::DNA::new("X").err(), Some(0));
@@ -24,7 +21,6 @@ fn test_invalid_dna_input() {
 }
 
 #[test]
-#[ignore]
 fn test_invalid_rna_input() {
     // Invalid character
     assert!(dna::RNA::new("X").is_err());
@@ -35,7 +31,6 @@ fn test_invalid_rna_input() {
 }
 
 #[test]
-#[ignore]
 fn test_acid_equals_acid() {
     assert_eq!(dna::DNA::new("CGA").unwrap(), dna::DNA::new("CGA").unwrap());
     assert_ne!(dna::DNA::new("CGA").unwrap(), dna::DNA::new("AGC").unwrap());
@@ -44,7 +39,6 @@ fn test_acid_equals_acid() {
 }
 
 #[test]
-#[ignore]
 fn test_transcribes_cytosine_guanine() {
     assert_eq!(
         dna::RNA::new("G").unwrap(),
@@ -53,7 +47,6 @@ fn test_transcribes_cytosine_guanine() {
 }
 
 #[test]
-#[ignore]
 fn test_transcribes_guanine_cytosine() {
     assert_eq!(
         dna::RNA::new("C").unwrap(),
@@ -62,7 +55,6 @@ fn test_transcribes_guanine_cytosine() {
 }
 
 #[test]
-#[ignore]
 fn test_transcribes_adenine_uracil() {
     assert_eq!(
         dna::RNA::new("U").unwrap(),
@@ -71,7 +63,6 @@ fn test_transcribes_adenine_uracil() {
 }
 
 #[test]
-#[ignore]
 fn test_transcribes_thymine_to_adenine() {
     assert_eq!(
         dna::RNA::new("A").unwrap(),
@@ -80,7 +71,6 @@ fn test_transcribes_thymine_to_adenine() {
 }
 
 #[test]
-#[ignore]
 fn test_transcribes_all_dna_to_rna() {
     assert_eq!(
         dna::RNA::new("UGCACCAGAAUU").unwrap(),
