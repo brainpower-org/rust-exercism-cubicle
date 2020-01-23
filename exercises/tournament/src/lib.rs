7ce1d221bdf0102ca2e238889224b9371bd3e464
rsteube <rsteube@users.noreply.github.com>
2020-01-23 17:34:49 +0100

    tournament WIP

--- exercises/tournament/src/lib.rs
+++ exercises/tournament/src/lib.rs
@@ -1,6 +1,44 @@
+use std::collections::HashMap;
+
 pub fn tally(match_results: &str) -> String {
-    unimplemented!(
-        "Given the result of the played matches '{}' return a properly formatted tally table string.",
-        match_results
-    );
+    let result_header =  "Team                           | MP |  W |  D |  L |  P".to_owned();
+
+    let results: HashMap<&str, (i32, i32, i32)> = HashMap::new();
+
+    let result_body = match_results.lines().fold(results, |mut acc, line| {
+        let m: Vec<&str> =line.split(';').collect();
+        let home_team = acc.entry(m[0]).or_insert((0,0,0));
+
+        match m[2] {
+            "win" => {
+                home_team.0 += 1;
+            },
+            "draw" => {
+                home_team.1 += 1;
+            }
+            "loss" => {
+                home_team.2 += 1;
+            }
+            _ => {}
+        };
+        let guest_team = acc.entry(m[1]).or_insert((0,0,0));
+
+        match m[2] {
+            "win" => {
+                guest_team.2 += 1;
+            },
+            "draw" => {
+                guest_team.1 += 1;
+            }
+            "loss" => {
+                guest_team.0 += 1;
+            }
+            _ => {}
+        };
+        dbg!(acc)
+    }).into_iter().collect::<Vec<(&str, (i32, i32, i32))>>().sort_by(|a, b| { (a.0 * 3 + a.1)b)}).into_iter().map(|(key, value)| {
+        format!("{0:<29}  |  {1} |  {2} |  {3} |  {4} |  {5}", key, (value.0 + value.1 + value.2), value.0, value.1, value.2, value.0 * 3 + value.1)
+    }).collect::<Vec<String>>().join("\n");
+
+    [result_header, result_body].join("\n")
 }
--- exercises/tournament/tests/tournament.rs
+++ exercises/tournament/tests/tournament.rs
@@ -1,7 +1,6 @@
 use tournament;
 
 #[test]
-#[ignore]
 fn just_the_header_if_no_input() {
     let input = "";
     let expected = "Team                           | MP |  W |  D |  L |  P";
@@ -10,7 +9,6 @@ fn just_the_header_if_no_input() {
 }
 
 #[test]
-#[ignore]
 fn a_win_is_three_points_a_loss_is_zero_points() {
     let input = "Allegoric Alaskans;Blithering Badgers;win";
     let expected = "Team                           | MP |  W |  D |  L |  P\n".to_string()
